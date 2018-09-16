f472e0d901fffdec0736e8a449c1fd2f34d1d7b6
Krzysztof Hamerszmit <kkhamer@gmail.com>
2018-09-16 21:29:01 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import { Header } from 'layouts/Header.js'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+        <Route path='/home' component={Home} />
+          {/* <Header/> */}
+          <div>
+            <ul>
+              <li>
+                <Link to='/home'>Home</Link>
+              </li>
+              <li>
+                <Link to='/'>name</Link>
+              </li>
+              <li>
+                <Link to='/'>name</Link>
+              </li>
+            </ul>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
---
+++ src/layouts/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from 'logo.svg'
+import 'App.css'
+
+export const Header = props => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Welcome to React</h1>
+  </header>
+)
+
