26bb0a5cae9bc762ede1594265cd623dcad5cc25
Krzysztof Hamerszmit <kkhamer@gmail.com>
2018-11-27 22:17:41 +0100

    Add Intro page

--- src/layouts/Pages/Intro.js
+++ src/layouts/Pages/Intro.js
@@ -1,15 +1,44 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 
 export const Intro = () => (
   <Fragment>
     <h1>React patterns roadmap</h1>
-    <div>
-      <ul>
-        <li>One</li>
-        <li>Two</li>
-      </ul>
-    </div>
+    <Wrapper>
+      <ListedSection header='On the run!' list={runItems} />
+      <ListedSection header='Backlog' list={backlogItems} />
+    </Wrapper>
   </Fragment>
 )
 
 export default Intro
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 15px;
+`
+
+const ListedSection = props => (
+  <section>
+    <div>
+      <h1>{props.header}</h1>
+      <ul>
+        {props.list.map(({ name }) => (
+          <li>{name}</li>
+        ))}
+      </ul>
+    </div>
+  </section>
+)
+
+const runItems = [
+  { name: 'ContextAPI', done: false },
+  { name: 'Hooks', done: false },
+  { name: 'HOC pattern', done: false },
+]
+
+const backlogItems = [
+  { name: 'Add redux', done: false },
+  { name: 'render props', done: false },
+]
