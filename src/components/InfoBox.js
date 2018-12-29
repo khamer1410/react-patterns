0628a585a93ddd22f473523cd284bee4648c3c2c
Krzysztof Hamerszmit <kkhamer@gmail.com>
2018-12-29 17:58:51 +0100

    Add InfoBox component

---
+++ src/components/InfoBox.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { THEME_CLASSIC, THEME_MODERN } from 'config/const'
+import { WithThemeConsumer } from 'utils/ThemeProvider'
+
+const defaultProps = {
+  theme: THEME_CLASSIC
+}
+
+const propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+  theme: PropTypes.string,
+}
+
+const InfoBox = ({ title, children, theme }) => (
+  <InfoWrapper theme={theme}>
+    {title && <Heading>{title}</Heading>}
+    {children}
+  </InfoWrapper>
+)
+
+InfoBox.defaultProps = defaultProps
+InfoBox.propTypes = propTypes
+
+export default WithThemeConsumer(InfoBox)
+
+// styled components
+const InfoWrapper = styled.div`
+  padding: 10px;
+  border-radius: 10px;
+  background: ${({ theme }) => {
+    const backgroundByTheme = {
+      [THEME_CLASSIC]: 'linear-gradient(rgba(255,255,255, 0.5), rgba(60,167,208, 0.5))',
+      [THEME_MODERN]: 'linear-gradient(rgba(255,255,255, 0.5), rgba(245,0,0, 0.5))',
+    }
+    return backgroundByTheme[theme]
+  }};
+`
+const Heading = styled.h4`
+  margin: 0;
+`
--- src/layouts/Pages/higherOrderComponent/DataPresenter.js
+++ src/layouts/Pages/higherOrderComponent/DataPresenter.js
@@ -1,26 +1,11 @@
 import React from 'react';
-
-import styled from 'styled-components'
-import { THEME_CLASSIC, THEME_MODERN } from 'config/const'
+import InfoBox from 'components/InfoBox'
 
 const DataPresenter = ({ theme }) => (
   <div>
-    <ThemePresenter theme={theme}>Current theme is <code>{theme}</code></ThemePresenter>
+    <InfoBox>Current theme is <code>{theme}</code></InfoBox>
     <h3>All correct, just displaying some funny animals {'\u{1F436}'} </h3>
   </div>
 )
-
-
-const ThemePresenter = styled.div`
-  padding: 10px;
-  border-radius: 6px;
-  background: ${({ theme }) => {
-    const backgroundByTheme = {
-      [THEME_CLASSIC]: 'linear-gradient(rgba(255,255,255, 0.5), rgba(60,167,208, 0.5))',
-      [THEME_MODERN]: 'linear-gradient(rgba(255,255,255, 0.5), rgba(245,0,0, 0.5))',
-    }
-    return backgroundByTheme[theme]
-  }};
-`
 
 export default DataPresenter
