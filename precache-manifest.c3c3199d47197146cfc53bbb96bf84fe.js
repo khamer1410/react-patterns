9d11b1e74588cae2c93d9cf466323157cc265651
Krzysztof Hamerszmit <kkhamer@gmail.com>
2019-01-06 15:06:50 +0100

    Updates

---
+++ precache-manifest.c3c3199d47197146cfc53bbb96bf84fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-patterns/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "abd9dc540791ddd8beacf86047bfdedb",
+    "url": "/react-patterns/static/media/blue-blank.abd9dc54.jpg"
+  },
+  {
+    "revision": "3d104213be04dfa3ce76",
+    "url": "/react-patterns/static/js/runtime~main.3d104213.js"
+  },
+  {
+    "revision": "1fb5c527f81aa23cba40",
+    "url": "/react-patterns/static/js/main.1fb5c527.chunk.js"
+  },
+  {
+    "revision": "c4bd0dd1c80d548f2999",
+    "url": "/react-patterns/static/js/1.c4bd0dd1.chunk.js"
+  },
+  {
+    "revision": "1fb5c527f81aa23cba40",
+    "url": "/react-patterns/static/css/main.6240e1d4.chunk.css"
+  },
+  {
+    "revision": "4d5bb695df8d5ae33c0a7904534fe397",
+    "url": "/react-patterns/index.html"
+  }
+];
