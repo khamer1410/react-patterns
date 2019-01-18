dd191f41b600b27dc74596dad60ae25749b9d310
Krzysztof Hamerszmit <kkhamer@gmail.com>
2019-01-18 23:57:56 +0100

    Move logo to assets

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from 'logo.svg'
+import logo from 'assets/logo.svg'
 import styled, { keyframes } from 'styled-components'
 
 import { CircleButton } from 'components/Buttons'
